086e3892a6d6652cc646bf664b47d4f2656267f8
Steffen Coenen <tar-dingens@Tar-PC.fritz.box>
2015-07-24 12:35:08 +0200

    Add myself to the list of copyright holders in README.md and integrate.js

--- integrate.js
+++ integrate.js
@@ -1,5 +1,6 @@
 /*
  * Copyright 2015 Stephen Herbein <stephen272@gmail.com>
+ * Copyright 2015 Steffen Coenen <steffen@steffen-coenen.de>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met: 
