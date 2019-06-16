c909d0e0f7d6bf17311e7db15b25c5b458aabbd2
Ajith R N <dev@ajithrn.com>
2019-06-17 03:13:26 +0530

    Update integrate.js
    Added Option to select India as a national variant

--- integrate.js
+++ integrate.js
@@ -41,7 +41,8 @@
     ['co.uk', C_('Amazon variant', 'United Kingdom')],
     ['com', C_('Amazon variant', 'United States')],
     ['it', C_('Amazon variant', 'Italy')],
-    ['ca', C_('Amazon variant', 'Canada')]
+    ['ca', C_('Amazon variant', 'Canada')],    
+    ['in', C_('Amazon variant', 'India')]
   ]
 
   var ACTION_THUMBS_UP = 'thumbs-up'
